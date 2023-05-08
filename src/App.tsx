2b64938fd8d9eb0f9244c051b8653a8817895292
Falase Femi <femifalase228@gmail.com>
2023-05-08 13:47:06 +0100

    resuseable components

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import Product from "./components/Product"
+import ProductShowCase from "./components/ProductShowCase"
 
 
 const App = () => {
   const [bgImage, setBgImage] = useState<string>('https://web-images.credcdn.in/_next/assets/images/home-page/video-bg-mobile.png')
+  const [prodImg, setProdImg] = useState<string>('https://web-images.credcdn.in/_next/assets/images/home-page/deserve-more-bg-mobile.jpg')
 
   useEffect(() => {
-    if (window.innerWidth >= 768) {
+    if (window.innerWidth >= 640) {
       setBgImage("https://web-images.credcdn.in/_next/assets/images/home-page/video-bg.png")
-
+    }
+    if (window.innerWidth >= 640) {
+      setProdImg('https://web-images.credcdn.in/_next/assets/images/home-page/deserve-more-bg.jpg')
     }
   }, []);
   return (
@@ -25,6 +29,10 @@ const App = () => {
         <Product />
       </section>
 
+      <section className="bg-rose-600 text-white w-screen md:min-h-screen relative pt-[195px] px-10 pb-[90px] min-h-[1150px] md:pt-60 md:px-[200px] md:pb-[180px] bg-cover flex flex-col justify-between" style={{ backgroundImage: `url(${prodImg})` }}>
+        <ProductShowCase text="feel special more often." anText="exclusive rewards for paying your bills" subText="every time you pay your credit card bills on CRED, you receive CRED coins. you can use these to win exclusive rewards or get special access to curated products and experiences. on CRED, good begets good.
+" />
+      </section>
     </div>
   )
 }
--- src/components/ProductShowCase.tsx
+++ src/components/ProductShowCase.tsx
@@ -1,8 +1,30 @@
-type Props = {}
+import Button from "../common/Button"
 
-const ProductShowCase = (props: Props) => {
+type Props = {
+text: string,
+anText: string
+className?: React.CSSProperties,
+subText?: string
+}
+
+const ProductShowCase = ({text,anText, className, subText}: Props) => {
   return (
-    <div>ProductShowCase</div>
+    <>
+    <div>
+        <div className="text-[65px] md:text-[100px] leading-[58px] md:leading-[96px] opacity-90 font-bold max-w-[700px]">
+            {text}
+        </div>
+        <div className="text-[18px] leading-6 md:text-[32px] font-bold md:leading-[38px] max-w-[630px] md:mt-4 mt-[26px]">
+            {anText}
+        </div>
+    </div>
+    <div>
+        <div className="md:text-lg md:leading-7 max-w-[630px] mb-10 font-semibold text-sm leading-5">
+            {subText}
+        </div>
+        <Button buttonText="Explore rewards" />
+    </div>
+    </>
   )
 }
 
